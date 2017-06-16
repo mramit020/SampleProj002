51438da9a902d73cbb5eeff9949ad62851a5637e
mramit020 <mramit020@gmail.com>
2017-06-16 08:54:44 +0530

    First Commit

---
+++ TestSuite/src/main/java/idaho/sit/testsuite/TestSuite/App.java
@@ -0,0 +1,13 @@
+package idaho.sit.testsuite.TestSuite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
